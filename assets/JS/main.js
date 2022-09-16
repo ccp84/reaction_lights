2e6e62cd4f48070909ae78d08cf1be471d49792a
ccp84 <ccp84@icloud.com>
2022-09-16 12:52:06 +0000

    Bug>Fix issues with undefined vairable and global vairable

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,33 +1,44 @@
 //Get a list of lights
 const lights = document.querySelectorAll('.light');
-lightOn(lights);
 
 //Wait for DOM content to be loaded
 document.addEventListener('DOMContentLoaded', function() {
   //run the game once loaded
-})
+  lightOn();
+});
 
 /*
-* Timer function used to randomise when the lights change from red to green between 0 and 2 seconds
+* Timer function used to randomise when the lights change from red to green between 1 and 3 seconds
 */
+let timer = 0;
 function lightTimer() {
-  return Math.random() * 2000;
+  timer = Math.random() * 3000;
+  if (timer <= 1000) {
+    timer = 1000;
+  }
+  return timer;
 }
 
 /*
 * Function to select a random light and switch the class to 'light on' then remove it again
 * after the specified time from lightTimer function
 */
-function lightOn(lights) {
-  //Get the number of lights currently on screen and generate a random number from this
+function lightOn() {
+  //Get the number of elements with class light and generate a random number from this
   let lightNumber = Math.floor(Math.random() * lights.length);
   let activeLight = lights[lightNumber];
-  
-  activeLight.classlist.add('light_on');
+  activeLight.classList.add('light_on');
+
   //The timeout feature was researched on StackOverflow - see link in credits
   //Turn light off again
+  let timeOut = 0;
   setTimeout(function() {
-    timeout = lightTimer();
-    activeLight.classlist.remove('light_on');
-  },timeOut);
+    timeOut = lightTimer();
+    lightOff(activeLight);
+  }, timeOut);
+}
+
+function lightOff(activeLight) {
+  activeLight.classList.remove('light_on');
+  lightOn();
 }
