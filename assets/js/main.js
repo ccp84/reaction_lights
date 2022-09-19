5d9980365deaca49f5edaf6b05d08fc43e6a40cf
ccp84 <ccp84@icloud.com>
2022-09-19 11:54:59 +0000

    Style>Further styling of rules modal box

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,16 +85,16 @@ function lightOff(activeLight) {
  * EXTERNAL CODE USED FROM https://www.w3schools.com/howto/howto_css_modals.asp
 */
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("rules_modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var rules = document.getElementById("rules");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+rules.onclick = function() {
   modal.style.display = "block";
 }
 
